597cc235e66f4450f68643fce447d4b3fa6fedd7
Ladislav Baco <laciKE3.14@gmail.com>
2015-01-01 16:44:09 +0100

    Add preprocessor to compile script, remove temp files after compilation

--- saral/compile.sh
+++ saral/compile.sh
@@ -7,9 +7,11 @@ DIR=`dirname $0`
 
 echo "Compiling $1 into $2"
 
-java -cp $DIR/bin:$CLASSPATH Compiler $1 > $1.ll && \
+$DIR/bin/preprocessor $1 > $1.prep
+java -cp $DIR/bin:$CLASSPATH Compiler $1.prep > $1.ll && \
 opt -S -std-compile-opts $1.ll > $1.opt.ll && \
 llvm-as $1.opt.ll && \
 llc $1.opt.bc -o $1.s && \
 #lli -load=$DIR/lib/libsaral.so $1.opt.bc
 gcc $DIR/lib/libsaral.o $1.s -o $2
+rm $1.prep $1.ll $1.opt.ll $1.opt.bc $1.s
